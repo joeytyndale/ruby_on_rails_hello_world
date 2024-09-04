36bdefe8fdd61e3cf1a2a5464738e8efbdd46dd2
Joseph Horning <joeytyndale@gmail.com>
2024-09-04 11:53:40 -0400

    layouts... nice

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "pages#home"
   get "about" => "pages#about"
+  get "contact" => "pages#contact"
 
   get "up" => "rails/health#show", as: :rails_health_check
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
